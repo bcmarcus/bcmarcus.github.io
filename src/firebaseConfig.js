ae83097002e4f566d40525862a4798febbffe0b7
Tacosaurus100 <bmarcus2003@gmail.com>
2023-07-15 19:40:00 -0700

    Temp commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,16 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyD7yrcF4hX7D4JClbtoU5Y4zS9BkfxH5kk",
+  authDomain: "call-to-action-2afc3.firebaseapp.com",
+  databaseURL: "https://call-to-action-2afc3-default-rtdb.firebaseio.com",
+  projectId: "call-to-action-2afc3",
+  storageBucket: "call-to-action-2afc3.appspot.com",
+  messagingSenderId: "643217092679",
+  appId: "1:643217092679:web:ff26c58b0c3c66cd020162",
+  measurementId: "G-VEZGEG7S0H"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
