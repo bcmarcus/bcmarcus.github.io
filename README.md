076edac86b406bb4b9150cc7e90e6ba339efe31e
Tacosaurus100 <bmarcus2003@gmail.com>
2023-10-21 15:01:56 -0700

    Changed Readme again

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Brandon Marcus's Github.io
 
 ## Work in Progress
-This project is currently under development.
+This project is currently under development. Soon, there will be demos of some projects I have worked on.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ const CircularDependencyPlugin = require('circular-dependency-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../docs'),
     filename: 'bundle.js',
     publicPath: '/',
   },
